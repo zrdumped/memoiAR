8815ccc5cdee9311bf2b7f10bafcbefddf50cadb
zrdumped <nmgzhangran@163.com>
2020-02-04 20:56:18 -0500

    Add network & concept 1 gold spike in progress

---
+++ Assets/Scripts/ProtobufMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProtoBuf;
+
+public class ProtobufMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ProtoContract]
+    public class Information
+    {
+        [ProtoMember(1)]
+        //0 - start program; 1 - start recording(b); 2 - start tracing(r); 3 - exit(e); -1 idle
+        public string testString { get; set; }
+    }
+}
